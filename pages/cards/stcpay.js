0d7fd5a8a2d96f374e54d0c4b7f79ad8f4fc4500
tareqqadi333 <tareqqadi1@outlook.sa>
2025-06-06 14:24:30 +0300

    أضفنا صفحات تفصيلية لكل بطاقة

---
+++ pages/cards/stcpay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function STCPayCard() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>بطاقة STC Pay الرقمية</h1>
+      <p>بطاقة مسبقة الدفع رقمية، تصدر فورياً من خلال تطبيق STC Pay وتعمل عبر شبكة Mastercard.</p>
+      <p>لا توجد رسوم سنوية</p>
+      <p>إصدار مباشر من التطبيق</p>
+    </main>
+  );
+}
+
